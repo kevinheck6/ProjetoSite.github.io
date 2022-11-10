f2499486b06043569058984d1751e91c60d4809f
kevinheck6 <kevinheck941972@gmail.com>
2022-11-10 19:26:04 -0300

    working

--- Projects/FileTransferSockets-Python/Transfer.py
+++ Projects/FileTransferSockets-Python/Transfer.py
@@ -1,18 +1,18 @@
 import os
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Internet Socket TCP socket 
-client.connect(("localhost", 9999)) # Can be any port number that`s free
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Internet Socket TCP socket
+client.connect(("localhost", 9999))  # Can be any port number that`s free
 
-file = open("images.png", "rb") # File reading in bytes mode 
-file_size = os.path.getsize("images.png")# Get the size of the file
+file = open("images.png", "rb")  # File reading in bytes mode
+file_size = os.path.getsize("images.png")  # Get the size of the file
 
-client.send("received_images.png".encode()) # Send the file name 
-client.send(str(file_size).encode()) # Send the file size
+client.send("received_images.png".encode())  # Send the file name
+client.send(str(file_size).encode())  # Send the file size
 
-data = file.read() #sending all the data 
+data = file.read()  # sending all the data
 client.sendall(data)
-client.send(b"<END>") # So we know when the file end and everthing after is a new file
+client.send(b"<END>")  # So we know when the file end and everthing after is a new file
 
-file.close() 
+file.close()
 client.close()
