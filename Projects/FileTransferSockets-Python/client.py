94dd4b57e0abf62f0247d163e8060568bd7f917f
kevinheck6 <kevinheck941972@gmail.com>
2022-11-12 12:27:27 -0300

    Working again

--- Projects/FileTransferSockets-Python/client.py
+++ Projects/FileTransferSockets-Python/client.py
@@ -18,7 +18,7 @@ print(f"The files that u can download are: {data}, which one would you like to d
 dl_file = input('')
 
 # Sending data to the server
-server.send(data.encode())
+server.send(dl_file.encode())
 
 # Receiving data size from server
 file_size = server.recv(1024).decode('utf-8')
@@ -30,7 +30,7 @@ while not done:
     print(f"Are you sure you want to download it? Type 'y' to download or 'n' to  exit")
     confirmation = input('')
     if confirmation == 'y':
-        server.send(data.encode())
+        server.send(dl_file.encode())
         done = True
 
     elif confirmation == 'n':
@@ -38,10 +38,10 @@ while not done:
         dl_file = input('')
 
     #### FAZER WHILE NISSO ENQUANTO CONFIRMATION NAO FOR DIFERENTE DE Y FAZER TAL PERGUNTA
-server.send(data.encode())
+
 
 # Write File in binary
-file = open(data, 'wb')
+file = open(dl_file, 'wb')
 
 # Keep receiving data from the server
 line = server.recv(1024)
