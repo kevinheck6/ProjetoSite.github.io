0dc6daf560cc5e1ade7d8a23a626c3bdad4f1802
kevinheck6 <kevinheck941972@gmail.com>
2022-11-12 12:02:31 -0300

    Not good

--- Projects/FileTransferSockets-Python/client.py
+++ Projects/FileTransferSockets-Python/client.py
@@ -15,9 +15,29 @@ print('Connection Established.')
 # Receiving data from server
 data = server.recv(1024).decode('utf-8')
 print(f"The files that u can download are: {data}, which one would you like to download?")
-data = input('')
+dl_file = input('')
 
 # Sending data to the server
+server.send(data.encode())
+
+# Receiving data size from server
+file_size = server.recv(1024).decode('utf-8')
+print(f"The size of the file is: {file_size} bytes")
+
+# confirmation
+done = False
+while not done:
+    print(f"Are you sure you want to download it? Type 'y' to download or 'n' to  exit")
+    confirmation = input('')
+    if confirmation == 'y':
+        server.send(data.encode())
+        done = True
+
+    elif confirmation == 'n':
+        print(f"The files that u can download are: {data}, which one would you like to download?")
+        dl_file = input('')
+
+    #### FAZER WHILE NISSO ENQUANTO CONFIRMATION NAO FOR DIFERENTE DE Y FAZER TAL PERGUNTA
 server.send(data.encode())
 
 # Write File in binary
@@ -26,7 +46,7 @@ file = open(data, 'wb')
 # Keep receiving data from the server
 line = server.recv(1024)
 
-while (line):
+while line:
     file.write(line)
     line = server.recv(1024)
 
--- Projects/FileTransferSockets-Python/server.py
+++ Projects/FileTransferSockets-Python/server.py
@@ -1,3 +1,4 @@
+import os
 import socket
 from os import listdir
 from os.path import isfile, join
@@ -10,7 +11,7 @@ print("Socket created successfully.")
 port = 8800
 host = ''
 
-# binding to the host and port
+# binding to the host and port, that`s what define it as a server
 server.bind((host, port))
 
 # Accepts up to 10 connections
@@ -21,19 +22,30 @@ while True:
     # Establish connection with the clients.
     client, addr = server.accept()
     print('Connected with ', addr)
+
     # Files names
     file_names = [f for f in listdir("files") if isfile(join("files", f))]
 
     # convert it to string, encode it and send it to the Receiver
     client.send(str(file_names).encode())
+
     # Get data from the client
     data = client.recv(1024).decode()
     path = "files/" + data
     print(data)
+
+    # Send file size to client
+    client.send(str(os.path.getsize(path)).encode())
+
+    # Get data from the client
+    data = client.recv(1024).decode()
+    path = "files/" + data
+    print(data)
+
     # Read File in binary
     file = open(path, 'rb')
-
     line = file.read(1024)
+
     # Keep sending data to the client
     while line:
         client.send(line)
